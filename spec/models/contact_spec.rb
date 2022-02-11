2a39529f11dc930235565a9d3b08435137c94f95
irizaga <gibran.irizaga@gmail.com>
2022-02-11 13:49:10 -0300

    "tests fixed"

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,26 +1,30 @@
 require_relative '../../config/environment'
 require_relative '../spec_helper'
 
-describe Contact, type: :model do
-  describe 'contact creation' do
-    before :all do
-      @user = User.create!(user_name: 'Joao')
-      @contact = @user.contacts.create!(c_name: 'Jose')
+RSpec.describe Contact, type: :model do
+  before :all do
+    @user = User.create!(user_name: 'Joao')
+    @contact = @user.contacts.create!(c_name: 'Jose')
+  end
+
+  after :all do
+    Contact.destroy_all
+    User.destroy_all
+  end
+
+  describe 'creation' do
+    context 'is successful' do
+      it 'given new contact name' do
+        contact = @user.contacts.new(c_name: 'Maria')
+        expect(contact).to be_valid
+      end
     end
 
-    after :all do
-      Contact.destroy_all
-      User.destroy_all
-    end
-
-    it 'is successful' do
-      contact = @user.contacts.new(c_name: 'Maria')
-      expect(contact).to be_valid
-    end
-
-    it 'fails' do
-      contact = @user.contacts.new(c_name: 'Jose')
-      expect(contact).not_to be_valid
+    context 'fails' do
+      it 'given existing name' do
+        contact = @user.contacts.new(c_name: 'Jose')
+        expect(contact).not_to be_valid
+      end
     end
   end
 end
