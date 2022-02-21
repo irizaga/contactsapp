ed99f02d817260a1ed902f9e7f5aa4f4e3e195f8
irizaga <gibran.irizaga@gmail.com>
2022-02-21 19:57:51 -0300

    "user creation method and tests added"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get 'users' => 'users#index'
-  #get 'users/show'
-  get 'users/new'
-  post 'users/create'
+  # get 'users/show'
+  # get 'users/new'
+  # post 'users/create'
   get 'users/edit'
   put 'users/update'
   delete 'users/destroy'
   root :to => 'users#index'
 
-  get '/userss/:id', to: 'users#show', as: 'user'
+  get 'users/new' => 'users#new'
+  post 'users' => 'users#create'
+  get 'users/:id' => 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
