df0001489517cdf5130494ef51c79ddcc7e29e34
irizaga <gibran.irizaga@gmail.com>
2022-02-21 10:11:37 -0300

    "added users info view"

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'users' => 'users#index'
-  get 'users/show'
+  #get 'users/show'
   get 'users/new'
   post 'users/create'
   get 'users/edit'
   put 'users/update'
   delete 'users/destroy'
   root :to => 'users#index'
+
+  get '/userss/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
